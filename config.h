874ddbb8eaec7161cf7c9656d3f035a5d41c58a3
gabo <gabriel.gaboardi13@gmail.com>
2020-06-22 11:55:05 -0300

    added fancybar patch

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "undefined:size=10" };
-static const char dmenufont[]       = "undefinded:size=10";
+static const char *fonts[]          = { "undefined:size=11" };
+static const char dmenufont[]       = "undefinded:size=12";
 static const char col_gray1[]       = "#240041";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#8d659c";
@@ -78,7 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,       {.v = lockcmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,       	 {.v = lockcmd } },
 	// { MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
