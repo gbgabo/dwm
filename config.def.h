1b4015c9a0d1719fcda8b204af0613d122410b52
gabo <gabriel.gaboardi13@gmail.com>
2020-08-03 20:40:56 -0300

    chenged text color, updated config.def

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "undefined:size=11" };
+static const char dmenufont[]       = "undefinded:size=12";
+static const char col_gray1[]       = "#240041";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#8d659c";
+static const char col_gray4[]       = "#00ffb7";
+static const char col_cyan[]        = "#900048";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -27,8 +27,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp"				,  NULL,       NULL,       0,            1,           -1 },
+	{ "code"				,  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "quterowser"	,  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "firefox"			,  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Zathura"			,  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "discord"			,  NULL,       NULL,       1 << 3,       0,           -1 },
+
 };
 
 /* layout(s) */
@@ -44,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,23 +61,36 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -fn undefined", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *tsearchcmd[] = {"tsearch-dmenu", NULL };
+static const char *settingsmenu[] = {"tmenu", "/home/gabo/menus/settings.yaml", NULL };
+static const char *lockcmd[] = {"i3lock-fancy", NULL };
+
+/* brightness + volume control*/
+static const char *brightnessup[] = {"brightness-ctrlr.sh", "-s", "5+", NULL };
+static const char *brightnessdown[] = {"brightness-ctrlr.sh", "-s", "5-", NULL };
+static const char *volumeup[] = {"volume-ctrlr.sh", "5%+", NULL };
+static const char *volumedown[] = {"volume-ctrlr.sh", "5%-", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,            						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_x,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_z,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,       	 {.v = lockcmd } },
+	// { MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -84,6 +102,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_d, 		 spawn,          {.v = tsearchcmd } },
+	{ MODKEY,						            XK_9, 		 spawn,          {.v = settingsmenu } },
+	{ MODKEY|ControlMask,						XK_Right,  spawn,          {.v = brightnessup } },
+	{ MODKEY|ControlMask,						XK_Left, 	 spawn,          {.v = brightnessdown } },
+	{ MODKEY|ControlMask,						XK_Up, 		 spawn,          {.v = volumeup } },
+	{ MODKEY|ControlMask,						XK_Down, 	 spawn,          {.v = volumedown } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -92,9 +116,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	// TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 };
 
 /* button definitions */
--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -9,7 +9,7 @@ static const char *fonts[]          = { "undefined:size=11" };
 static const char dmenufont[]       = "undefinded:size=12";
 static const char col_gray1[]       = "#240041";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#8d659c";
+static const char col_gray3[]       = "#c79bff";
 static const char col_gray4[]       = "#00ffb7";
 static const char col_cyan[]        = "#900048";
 static const char *colors[][3]      = {
@@ -61,11 +61,18 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "j4-dmenu-desktop", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -fn undefined", NULL };
-static const char *termcmd[]  = { "terminology", NULL };
-static const char *tsearchcmd[] = {"/home/gabo/Scripts/pytools/tsearch/tsearch-dmenu.sh", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *tsearchcmd[] = {"tsearch-dmenu", NULL };
+static const char *settingsmenu[] = {"tmenu", "/home/gabo/menus/settings.yaml", NULL };
 static const char *lockcmd[] = {"i3lock-fancy", NULL };
+
+/* brightness + volume control*/
+static const char *brightnessup[] = {"brightness-ctrlr.sh", "-s", "5+", NULL };
+static const char *brightnessdown[] = {"brightness-ctrlr.sh", "-s", "5-", NULL };
+static const char *volumeup[] = {"volume-ctrlr.sh", "5%+", NULL };
+static const char *volumedown[] = {"volume-ctrlr.sh", "5%-", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,6 +81,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_x,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_z,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -93,7 +102,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_d, spawn,         {.v = tsearchcmd } },
+	{ MODKEY|ShiftMask,             XK_d, 		 spawn,          {.v = tsearchcmd } },
+	{ MODKEY,						            XK_9, 		 spawn,          {.v = settingsmenu } },
+	{ MODKEY|ControlMask,						XK_Right,  spawn,          {.v = brightnessup } },
+	{ MODKEY|ControlMask,						XK_Left, 	 spawn,          {.v = brightnessdown } },
+	{ MODKEY|ControlMask,						XK_Up, 		 spawn,          {.v = volumeup } },
+	{ MODKEY|ControlMask,						XK_Down, 	 spawn,          {.v = volumedown } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -102,7 +116,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	// TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 };
 
